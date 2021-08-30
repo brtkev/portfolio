c26f5e24faf6b1245a34cffad9d81f51c1081a86
kev <kbreto2911@gmail.com>
2021-08-30 18:13:47 -0400

    gridbox update

--- src/app.js
+++ src/app.js
@@ -1,5 +1,47 @@
-const textImageCovers = document.querySelectorAll('.project__image-cover p');
-
+const projects = [
+    {
+        'name' : 'google clone',
+        'url' : '#',
+        'repo' : '#',
+        'display' : 'assets/google-clone.png'
+    },
+    {
+        'name' : 'google clone',
+        'url' : '#',
+        'repo' : '#',
+        'display' : 'assets/google-clone.png'
+    },
+    {
+        'name' : 'google clone',
+        'url' : '#',
+        'repo' : '#',
+        'display' : 'assets/google-clone.png'
+    },
+    {
+        'name' : 'google clone',
+        'url' : '#',
+        'repo' : '#',
+        'display' : 'assets/google-clone.png'
+    },
+    {
+        'name' : 'google clone',
+        'url' : '#',
+        'repo' : '#',
+        'display' : 'assets/google-clone.png'
+    },
+    {
+        'name' : 'google clone',
+        'url' : '#',
+        'repo' : '#',
+        'display' : 'assets/google-clone.png'
+    },
+    {
+        'name' : 'google clone',
+        'url' : '#',
+        'repo' : '#',
+        'display' : 'assets/google-clone.png'
+    },
+]
 
 /**
  * 
@@ -10,33 +52,49 @@ const textImageCovers = document.querySelectorAll('.project__image-cover p');
 const addEventListenerToArray = ( array, type, callable) => {
     for(let i = 0; i < array.length; i++){
         array[i].addEventListener(type, callable);
-
+        
     }
 }
 
 
 document.body.onload = () => {
-
-    addEventListenerToArray(textImageCovers, 'click', magic);
-    projectPreview()
+    
+    insertProjectItems();
+    const textImageCovers = document.querySelectorAll('.preview-icon');
+    addEventListenerToArray(textImageCovers, 'click', () => console.log("uwu"));
 }
 
-/**
- * 
- * @param {Event} ev 
- */
-function magic(ev){
-    console.log(ev.currentTarget.parentElement.parentElement);
+const insertProjectItems = () => {
+    let itemHTML = `
+    <li class="project__grid-item">
+        <img class="project__image" src="" alt="" >
+        <div class="project__image-cover">
+            <div class="text-box">
+                <p></p>
+            </div>
+            <a class="preview-icon" href="#"><img src="assets/preview-icon.png" alt="preview icon"></a>
+            <a class="code-icon" href="#"><img  src="assets/code-logo.png" alt="preview icon"></a>
+        </div>
+    </li>
+    `;
+    itemHTML = itemHTML.trim();
 
-    /**@type {Element} */
-    let listItem = ev.currentTarget.parentElement.parentElement;
-    listItem.classList.add('project__focused-li')
-}
+    const projectsNodes = projects.map( ( project ) => {
+        const template = document.createElement('template');
+        template.insertAdjacentHTML('afterbegin', itemHTML);
+
+        template.querySelector('.text-box p').insertAdjacentText('afterbegin', project.name);
+        template.querySelector('.preview-icon').href = project.url;
+        template.querySelector('.code-icon').href = project.repo;
+        let image = template.querySelector('.project__image');
+        image.src = project.display;
+        image.alt = project.name;
+
+        return template.firstElementChild;
+    })
+
+    const grid = document.getElementById("project__grid-box");
+    grid.append(...projectsNodes);
 
 
-const projectPreview = () => {
-    const previewContainer = document.getElementById('project__display-container');
-    const image = document.getElementsByClassName('project__image')[0];
-    previewContainer.children[0].appendChild(image.cloneNode());
-    previewContainer.children[1].children[0].textContent = image.nextElementSibling.firstElementChild.textContent;
 }
